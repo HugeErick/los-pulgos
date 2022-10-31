89902bf15dc35624742a1cd3ef005e5b617272fc
tr0ncoduro <erick.parada101@gmail.com>
2022-10-31 17:03:01 -0600

    more hw

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) =>  {
        console.log(entry)
        if (entry.isIntersecting) {
-        entry.target.classList.add('show');
+        entry.target.classList.add('show2');
        }  else {
-        entry.target.classList.remove('show');
+        entry.target.classList.remove('show2');
        }
     });
 });
-const hiddenElements = document.querySelectorAll('.hidden');
+const hiddenElements = document.querySelectorAll('.hidden2');
 hiddenElements.forEach((el) => observer.observe(el));
