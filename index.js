f4d65baf1d5a7c66d4fdccb1fe9dd0c245d7be41
tr0ncoduro <erick.parada101@gmail.com>
2022-11-14 15:38:28 -0600

    remove fake coockie

--- index.js
+++ index.js
@@ -1,6 +1,1 @@
-const popUp = document.getElementById('pop-up');
-const closeBtn = document.getElementById('close-btn1');
 
-closeBtn.addEventListener('click', () => {
-    popUp.classList.remove('b-show');
-});
