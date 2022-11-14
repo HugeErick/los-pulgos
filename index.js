2ebcf9948864f9c77a3a1f8b1bfd59f0351a7a44
tr0ncoduro <erick.parada101@gmail.com>
2022-11-14 11:21:28 -0600

    fixed homepage

--- index.js
+++ index.js
@@ -4,16 +4,3 @@ const closeBtn = document.getElementById('close-btn1');
 closeBtn.addEventListener('click', () => {
     popUp.classList.remove('b-show');
 });
-
-const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) =>  {
-       console.log(entry)
-       if (entry.isIntersecting) {
-        entry.target.classList.add('show2');
-       }  else {
-        entry.target.classList.remove('show2');
-       }
-    });
-});
-const hiddenElements = document.querySelectorAll('.hidden2');
-hiddenElements.forEach((el) => observer.observe(el));
